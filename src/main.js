f6c06f4ab7413f2d0feb42479ece8f7e62dda63e
mpilo.dlamini@yahoo.com <mpilo.dlamini@yahoo.com>
2019-06-23 08:04:14 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+// Vue.directive("autosize", {
+//   bind(el){ autosize(el) },
+//   inserted(el) { autosize.update(el) },
+//   update(el){ autosize.update(el) },
+//   unbind(el){ autosize.destroy(el) }
+// });
+
+Vue.directive('highlight', {
+  bind(el, binding, vnode) {
+    //el.style.backgroundColor = 'green';
+    // el.style.backgroundColor = binding.value;
+    var delay = 0;
+    if(binding.modifiers['delayed']){
+      delay = 3000;
+    }
+    setTimeout(() => {
+      if(binding.arg == 'background') {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay);
+    
+  }
+});
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
